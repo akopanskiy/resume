3a1f6069a53e116d8f33a10a8b47720aac5dd570
akopanskiy <andrii.kopanskyi@hevelian.com>
2025-04-15 16:17:48 +0300

    resume

---
+++ cv/src/utils/variables.js
@@ -0,0 +1,32 @@
+export const TECH_SKILLS = [
+	{ name: "Languages", item: "JavaScript, TypeScript" },
+	{ name: "Frameworks & Libraries", item: "Vue.js, React.js, Next.js, Vuex, Pinia, Vue-Router" },
+	{ name: "Build Tools", item: "Webpack, Vite" }, { name: "Visualization", item: "ECharts, D3.js" },
+	{ name: "Styling", item: " HTML5, CSS3, SCSS/SASS, Element-UI, Element-Plus" },
+	{ name: "Testing", item: "Mocha, Vitest, Sinon" }, { name: "Date & Time Libraries:", item: "Moment.js, Day.js" },
+	{ name: "Version Control", item: " Git, Bitbucket, GitHub" }, { name: "Task Management", item: "Jira" },
+	{ name: "Design & Prototyping", item: "Figma" }, { name: "Development Approach", item: "Agile, Responsive/Adaptive design" }
+];
+
+export const SOFT_SKILLS = [ "Responsibility", "Communication", "Teamwork", "Multitasking", "Flexibility", "Analytical thinking",
+	"Problem-solving mindset"];
+
+export const PROJECTS = [
+	{
+		name: "Medical Platform - Children's Hospital of Minnesota (USA)",
+		stack: "JavaScript, Vue.js, Vuex, Vue-Router, day.js, Websocket, Element-UI, SCSS, Webpack",
+		item: [`Developed new features and improved existing functionality in a Vue.js-based SPA`,
+			`Handled data related to patients' visits, diagnoses, and examinations`,
+			`Communicated directly with product managers and designers`,
+			`Maintained and optimized SCSS styles using BEM methodology`]
+	},
+	{
+		name: "Hybrid Integration Platform",
+		stack: "JavaScript, TypeScript, Vue.js, Vuex, Vue-Router, moment.js/day.js, ECharts, D3, Mocha, Sinon, " +
+				"Websocket, Element-Plus, SCSS, Vite, Pinia, Vitest",
+		item: ["Participated in the development of multiple SPAs from scratch and supported existing ones",
+			"Created dynamic dashboards and data visualizations using ECharts and D3",
+			"Refactored legacy code for better maintainability and performance",
+			"Collaborated with backend developers and QA team to align on API structure",
+			"Wrote unit tests with Mocha, Vitest, and Sinon to ensure code reliability"]
+	}];
